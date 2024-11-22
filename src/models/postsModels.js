2ce2abc94f22194a8e42c8b45d87ed20af9d18fe
Sab <skiirford@gmail.com>
2024-11-22 09:50:00 -0300

    quarta aula

--- src/models/postsModels.js
+++ src/models/postsModels.js
@@ -9,3 +9,9 @@ export async function getTodosPosts(){
     const colecao = db.collection("posts");
     return colecao.find().toArray();
 }
+
+export async function criarPost(novoPost){
+    const db = conexao.db("imersao-instabytes");
+    const colecao = db.collection("posts");
+    return colecao.insertOne(novoPost);
+}
