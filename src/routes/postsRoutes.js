46fed7f2a2dc981543e6b4bd3698331499088301
Sab <skiirford@gmail.com>
2024-11-22 09:55:56 -0300

    ajuste para ter comentários explicativos

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -2,10 +2,13 @@ import express from "express";
 import multer from "multer";
 import { listarPosts, postarNovoPost, uploadImagem } from "../controllers/postsController.js";
 
+// Configura o armazenamento Multer para o upload de imagens
 const storage = multer.diskStorage({
+    // Define o diretório de destino para os arquivos
     destination: function (req, file, cb) {
         cb(null, 'uploads/');
     },
+    // Define o nome do arquivo
     filename: function (req, file, cb) {
         cb(null, file.originalname);
     }
@@ -13,6 +16,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({ dest: "./uploads" , storage})
 
+// Define as rotas usando o objeto Express app
 const routes = (app) => {
     // Permite que o aplicativo entenda dados enviados no formato JSON
     app.use(express.json());
